9bddf9a178912d8e4588d30c95de09a1119a29f8
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-02 18:23:40 +0100

    Create reflection.go

---
+++ reflection.go
@@ -0,0 +1,17 @@
+type Person struct {
+    Name string
+    Age  int
+}
+
+func printFields(v interface{}) {
+    value := reflect.ValueOf(v)
+    for i := 0; i < value.NumField(); i++ {
+        field := value.Field(i)
+        fmt.Printf("%s = %v\n", reflect.TypeOf(field).Name(), field.Interface())
+    }
+}
+
+func main() {
+    p := Person{Name: "John", Age: 30}
+    printFields(p)
+}
