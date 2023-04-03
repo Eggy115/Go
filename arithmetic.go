6207803d92c19fc3c5e7f4664d94d21c1ca2c45f
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:25:45 +0100

    Create arithmetic.go

---
+++ arithmetic.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+func main() {
+
+  num1 := 6
+  num2 := 2
+
+  // + adds two variables
+  sum := num1 + num2
+  fmt.Printf("%d + %d = %d\n", num1, num2, sum)
+
+  // - subtract two variables
+  difference := num1 - num2
+  fmt.Printf("%d - %d = %d\n",num1, num2,  difference)
+
+  // * multiply two variables
+  product := num1 * num2
+  fmt.Printf("%d * %d is %d\n",num1, num2,  product)
+
+}
