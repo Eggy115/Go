236ac76818467035085d753175f0329b30e07722
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:22:48 +0100

    Create float.go

---
+++ float.go
@@ -0,0 +1,18 @@
+// Program to illustrate float32 and float64 with example
+
+package main
+import "fmt"
+
+func main() {
+  var salary1 float32
+  var salary2 float64
+
+  salary1 = 50000.503882901
+
+  // can store decimals with greater precision
+  salary2 = 50000.503882901
+
+  fmt.Println(salary1) 
+  fmt.Println(salary2)
+
+}
