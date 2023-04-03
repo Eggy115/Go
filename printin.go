0448e168ca2e76ce1baaed531681e3a8ecc16904
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:24:04 +0100

    Create printin.go

---
+++ printin.go
@@ -0,0 +1,15 @@
+// Program to illustrate the use of Println() function
+
+package main
+import "fmt"
+
+// prints output in different lines
+func main() {
+
+  currentSalary := 50000
+
+  fmt.Println("Hello")
+  fmt.Println("World!")
+  fmt.Println("Current Salary:", currentSalary)
+
+}
