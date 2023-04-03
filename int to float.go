cfb858edd57f41278cf26718828b1dd8c9206587
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:28:33 +0100

    Create int to float.go

---
+++ int to float.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+  
+  var intValue int = 2
+
+  // type conversion from int to float
+  var floatValue float32 = float32(intValue)
+ 
+  
+  fmt.Printf("Integer Value is %d\n", intValue)
+  fmt.Printf("Float Value is %f", floatValue)
+
+}
