84229285406bfc3c0b95ce5e39f223be4060a38c
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:26:20 +0100

    Create division.go

---
+++ division.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main() {
+
+  num1 := 11
+  num2 := 4
+
+  // / divide two integer variables
+  quotient := num1 / num2
+  fmt.Printf(" %d / %d = %d\n", num1, num2, quotient)
+
+}
