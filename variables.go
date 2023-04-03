0b459ec3f6fdaf4d84c9a2897f4c0faa9472e794
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:22:19 +0100

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main() {
+ 
+  // explicitly declare the data type
+  var number1 int = 10
+  fmt.Println(number1)
+
+ // assign a value without declaring the data type
+  var number2 = 20
+  fmt.Println(number2)
+
+  // shorthand notation to define variable
+  number3 := 30
+  fmt.Println(number3)  
+}
