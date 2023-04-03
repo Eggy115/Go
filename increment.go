3e1b4d847c667503c8fac6f43833397e79febecd
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:26:46 +0100

    Create increment.go

---
+++ increment.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+
+  num := 5
+
+  // increment of num by 1
+  num++
+  fmt.Println(num)  // 6
+
+  // decrement of num by 1
+  num--
+  fmt.Println(num)  // 4
+
+}
