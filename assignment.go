e74b94939ada1189af3311475a356dad12cca7bb
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-03 15:26:58 +0100

    Create assignment.go

---
+++ assignment.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+
+  num := 6
+  var result int
+
+  // = operator to assign the value of num to result
+  result = num
+  fmt.Println(result)    // 6
+}
